f1de3b1b4be6386869db18634211d656cfd21eb8
Rafaella Cristina <rsacramento@osas.com.br>
2023-09-03 23:05:32 -0300

    implementando metodo cobrarAluno

--- Implementacao/Contabilidade.java
+++ Implementacao/Contabilidade.java
@@ -1,7 +1,23 @@
 public class Contabilidade extends Usuario{
+    /**
+     * @param nome
+     * @param email
+     * @param senha
+     * @param cpf
+     */
     public Contabilidade(String nome, String email, String senha, String cpf) {
         super(nome, email, senha, cpf);
     }
 
-    public void cobrarAlunos() {}
+    /**
+     * @param aluno 
+     */
+    public void cobrarAluno( Aluno aluno, String textoDaNotificacao ) {
+        aluno.setNotificacoes(textoDaNotificacao);
+    }
+
+    @Override
+    public String toString() {
+        return "Contabilidade []";
+    }
 }
