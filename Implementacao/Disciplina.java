4ddebce66fbf5c5cb4a42e7ac2aea02b900f9c82
Rafaella Cristina <rsacramento@osas.com.br>
2023-09-05 00:33:57 -0300

    craindo construtor para disciplina

--- Implementacao/Disciplina.java
+++ Implementacao/Disciplina.java
@@ -4,5 +4,19 @@ public class Disciplina{
     private boolean obrigatoria;  
     private boolean ativa;  
 
+    Anexo anexo = Anexo.getInstance();
+    private Disciplina(String nome, int id, boolean obrigatoria, boolean ativa){
+        this.nome = nome;
+        this.id = id;
+        this.obrigatoria = obrigatoria;
+        this.ativa = ativa;
+    }
+
+    public void criarDisciplina(String nome, int id, boolean obrigatoria, boolean ativa){
+        Disciplina disciplina = new Disciplina(nome, id, obrigatoria, ativa);
+    }
     public void encerrarMatriculas() {};
+    public void setDisciplinas(){
+        anexo.printarConteudoAnexoNoConsole("Disciplinas");
+    }
 }
